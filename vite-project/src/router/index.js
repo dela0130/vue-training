2cc737807976092dad23c61559be4a9dc1c7f2f5
Charles K. Delahunt <90341103+dela0130@users.noreply.github.com>
2023-03-29 16:14:31 -0400

    Finished Vue Router

--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -1,10 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
 
 const routes = [
     {path: '/', name:'Home', component: Home},
-    {path: '/about', name:'About', component: About}
+    {
+        path: '/destination/:id:slug', 
+        name:'destination.show', 
+        component: ()=>import('@/views/DestinationShow.vue'),
+        props: route=> ({...route.params, id: parseInt(route.params.id)}),
+        children: [
+            {
+                path: ':experienceSlug',
+                name: 'experience.show',
+                component: () => import('@/views/ExperienceShow.vue'),
+                props: route=> ({...route.params, id: parseInt(route.params.id)})
+            }
+        ]
+    },
+    
 ]
 
 const router = createRouter({
